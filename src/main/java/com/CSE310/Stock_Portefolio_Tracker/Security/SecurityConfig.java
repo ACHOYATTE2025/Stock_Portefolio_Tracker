293802123de9fbcdf29d55de38b6e8fd2a0a25cc
ACHO YATTE DEIVY CONSTANT <acho.quebec@gmail.com>
2026-01-13 01:42:48 +0000

    new start

---
+++ src/main/java/com/CSE310/Stock_Portefolio_Tracker/Security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.CSE310.Stock_Portefolio_Tracker.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain FilterChain (HttpSecurity httpSecurity) throws Exception{
+
+        return httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize->
+                authorize
+                    .requestMatchers("/h2-console/**").permitAll() // autorise H2
+                    .anyRequest().permitAll())
+            .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+            .build();
+    }
+
+    
+@Bean
+public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    
+
+
+}
---
+++ src/main/java/com/CSE310/Stock_Portefolio_Tracker/StockPortefolioTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.CSE310.Stock_Portefolio_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPortefolioTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortefolioTrackerApplication.class, args);
+	}
+
+}
